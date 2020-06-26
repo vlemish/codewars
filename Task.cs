ce8ffb887c68b763a406523e1fd698d0b9dc2fb1
MdmaSteel <vlad.lemish123@gmail.com>
2020-06-26 21:53:13 +0300

    added new tasks

--- Task.cs
+++ Task.cs
@@ -278,23 +278,23 @@ namespace CodeWarsSolutions
             b = b < 0 ? 0 : b > 255 ? 255 : b;
 
             string rStr = r < 15 && r > 0 ? "0" : null;
-            rStr+= ToHex(r, r == 0 ? "00" : null);
+            rStr += ToHex(r, r == 0 ? "00" : null);
             string gStr = g < 15 && g > 0 ? "0" : null;
-            gStr+= ToHex(g, g == 0 ? "00" : null);
+            gStr += ToHex(g, g == 0 ? "00" : null);
             string bStr = b < 15 && b > 0 ? "0" : null;
             bStr += ToHex(b, b == 0 ? "00" : null);
 
             return string.Join("", rStr, gStr, bStr);
         }
 
-        public static string ToHex(int value, string hex=null)
+        public static string ToHex(int value, string hex = null)
         {
             if (value <= 0 && hex != null)
             {
-                return string.Join("",hex.Reverse());
+                return string.Join("", hex.Reverse());
             }
 
-            int divided   = value / 16;
+            int divided = value / 16;
             int remainder = value % 16;
 
 
@@ -346,5 +346,528 @@ namespace CodeWarsSolutions
         }
 
         #endregion
+
+        #region https://www.codewars.com/kata/596fba44963025c878000039/
+        public static string Contamination(string text, string character)
+        {
+            return text.Length >= 0 || character.Length >= 0 ? string.Join("", text.Select(x => character)) : string.Empty;
+        }
+        #endregion
+
+        #region https://www.codewars.com/kata/55908aad6620c066bc00002a/
+        public static bool XO(string input)
+        {
+            var x = input.ToLower().Where(i => i == 'x').Count();
+            var o = input.ToLower().Where(i => i == 'o').Count();
+            return x == o;
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/54df2067ecaa226eca000229/
+        public static long? RangeSum(long n)
+        {
+            if (n <= 0)
+            {
+                return null;
+            }
+            return (n * (n + 1) / 2);
+
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/57f609022f4d534f05000024/
+        public static int Stray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                int temp = numbers[i];
+                if (temp != numbers[0] && temp != numbers[1])
+                {
+                    return temp;
+                }
+            }
+
+            return numbers[0];
+
+        }
+        #endregion
+
+        #region https://www.codewars.com/kata/5526fc09a1bbd946250002dc/
+        public static int Find(int[] integers)
+        {
+            var odd = integers.Where(i => i % 2 == 0).ToArray();
+            var even = integers.Where(i => i % 2 != 0).ToArray();
+            return odd.Length > even.Length ? even[0] : odd[0];
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/
+        public static string RevRot(string strng, int sz)
+        {
+            Console.WriteLine($"{strng}, {sz}");
+            List<string> list = new List<string>();
+            int length = strng.Length;
+            string output = "";
+            if (sz <= 0 || sz > strng.Length) { return ""; }
+            while (length != 0 && length >= sz)
+            {
+                string temp = strng.Substring(0, sz);
+                list.Add(temp);
+                strng = strng.Remove(0, sz);
+                length -= sz;
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                var temp2 = list[i];
+                int sum = 0;
+                foreach (var el in temp2)
+                {
+                    sum += (int)Math.Pow((int)el - 48, 3);
+                }
+                if (sum % 2 == 0)
+                {
+                    char[] arr = temp2.ToCharArray();
+                    Array.Reverse(arr);
+                    output += new string(arr);
+                }
+                else
+                {
+                    char temp3 = temp2[0];
+                    output += $"{temp2.Remove(0, 1)}{temp3}";
+                }
+            }
+            return output.ToString();
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/52c31f8e6605bcc646000082/
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int temp;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    temp = numbers[i] + numbers[j];
+                    if (temp == target)
+                    {
+                        list.Add(i);
+                        list.Add(j);
+                        break;
+                    }
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/52efefcbcdf57161d4000091/
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> list = new Dictionary<char, int>();
+            char temp = ' ';
+            int count = 0;
+            var sortedStr = str.OrderBy(s => s).ToArray();
+            for (int i = 0; i < sortedStr.Length; i += count)
+            {
+                temp = sortedStr[i];
+                count = 0;
+                for (int j = 0; j < sortedStr.Length; j++)
+                {
+                    if (sortedStr[i] == sortedStr[j])
+                    {
+                        count++;
+                    }
+                }
+                list.Add(temp, count);
+            }
+            return list;
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/56606694ec01347ce800001b/
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            return a > 0 && b > 0 &&
+            a + b > c && a + c > b && b + c > a ?
+            true : false;
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/545cedaa9943f7fe7b000048/
+        public static bool IsPangram(string str)
+        {
+            char[] alphabet =
+           {
+                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+            };
+            if (str.Length >= 26)
+            {
+                str = str.ToLower();
+                int count = 0;
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        if (alphabet[i] == str[j])
+                        {
+                            Console.WriteLine($"letters : {alphabet[i]} and {str[j]} are the same!");
+                            count++;
+                            Console.WriteLine(count);
+                            break;
+                        }
+                    }
+                }
+                return count >= 26 ? true : false;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/
+        public static string MakeUpperCase(string str)
+        {
+            return str.ToUpper();
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/55eca815d0d20962e1000106/
+        public static int[] GenerateRange(int min, int max, int step)
+        {
+            List<int> arr = new List<int>();
+            for (; min <= max; min += step)
+            {
+                arr.Add(min);
+            }
+            return arr.ToArray();
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/5264d2b162488dc400000001
+        public static string SpinWords(string sentence)
+        {
+            var words = sentence.Split(' ');
+            string result = null;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] temp = words[i].ToCharArray();
+                if (temp.Length >= 5)
+                {
+                    Array.Reverse(temp);
+                    result += new string(temp);
+                }
+                else
+                {
+                    result += new string(temp);
+                }
+                result += " ";
+            }
+            result = result.Remove(result.LastIndexOf(" "), 1);
+            return result;
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/5388f0e00b24c5635e000fc6/
+        public class Swapper
+        {
+            public object[] Arguments { get; private set; }
+
+            public Swapper(object[] args)
+            {
+                Arguments = args;
+            }
+
+            public void SwapValues()
+            {
+
+                object temp = Arguments[0];
+                Arguments[0] = Arguments[1];
+                Arguments[1] = temp;
+            }
+
+        }
+        #endregion
+
+        #region https://www.codewars.com/kata/5772da22b89313a4d50012f7/
+        public static string Greet(string name, string owner)
+        {
+            if (name.Length != owner.Length) { return "Hello guest"; }
+            else
+            {
+                int count = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (name[i] == owner[i])
+                    {
+                        count++;
+                    }
+                }
+                if (count == name.Length) { return "Hello boss"; }
+            }
+            return "Hello guest";
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/5949481f86420f59480000e7/
+        public static string OddOrEven(int[] array)
+        {
+            int sum = 0;
+            string odd = "odd";
+            string even = "even";
+            if (array.Length < 1)
+            {
+                return even;
+            }
+            foreach (var i in array)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            if (sum % 2 == 0)
+            {
+                return even;
+            }
+            else { return odd; }
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/5839edaa6754d6fec10000a2/
+        public static char FindMissingLetter(char[] array)
+        {
+            char result = ' ';
+            int temp = Convert.ToInt32(array[0]);
+            for (int i = 1; i < array.Length; i++)
+            {
+                int temp1 = Convert.ToInt32(array[i]);
+                Console.WriteLine($"temp: {temp}\ntemp1: {temp1}");
+                int diff = temp1 - temp;
+                Console.WriteLine($"Difference between them: {diff}");
+                if (diff == 1)
+                {
+                    temp = temp1;
+                    Console.WriteLine("THERE ARE  NO DIFFERENCE!");
+                }
+                else
+                {
+                    result = Convert.ToChar(temp + 1);
+                    Console.WriteLine("THERE ARE DIFFERENCE!");
+                }
+
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/5626b561280a42ecc50000d1/
+        public class SumDigPower
+        {
+
+            public static long[] SumDigPow(long a, long b)
+            {
+                List<long> longs = new List<long>();
+                for (long i = a; i < b; i++) //every step we'll send some value [a...b] into method IsEureka, if we have true, we'll add this value to massive
+                {
+                    if (IsEureka(i))
+                    {
+                        longs.Add(i);
+                    }
+                }
+                return longs.ToArray();
+            }
+
+            public static bool IsEureka(long x)
+            {
+
+                string strX = Convert.ToString(x);
+                long sum = 0;
+                long temp = 0;
+                for (int i = 0; i < strX.Length; i++)
+                {
+                    temp = Convert.ToInt64(strX[i]) - 48;
+                    sum += (int)Math.Pow((temp), (i + 1));
+                }
+                if (x == sum)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/55685cd7ad70877c23000102/
+        public static int MakeNegative(int number)
+        {
+            return Math.Abs(number) * -1;
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/55c45be3b2079eccff00010f/
+        public static string Order(string words)
+        {
+            string finalString = "";
+            string stringTemp = "";
+            int intTemp = 0;
+            List<string> strings = new List<string>();
+            strings.AddRange(words.Split(' '));
+            List<int> numb = new List<int>();
+
+            for (int i = 0; i < strings.Count; i++)
+            {
+                string temp = strings[i];
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (Char.IsNumber(temp[j]))
+                    {
+                        numb.Add(temp[j]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < numb.Count; i++)
+            {
+
+                for (int j = i + 1; j < numb.Count; j++)
+                {
+                    if (numb[i] > numb[j])
+                    {
+                        intTemp = numb[i];
+                        stringTemp = strings[i];
+                        strings[i] = strings[j];
+                        numb[i] = numb[j];
+                        strings[j] = stringTemp;
+                        numb[j] = intTemp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < strings.Count - 1; i++)
+            {
+                finalString += $"{strings[i]} ";
+            }
+
+            return finalString += $"{strings[strings.Count - 1]}";
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/59752e1f064d1261cb0000ec/
+        public static string WhatTimeIsIt(double angle)
+        {
+            if (angle < 30)
+            {
+                angle += angle;
+                angle = (int)angle;
+                if (angle <= 9)
+                {
+                    return $"12:0{angle}";
+                }
+                return $"12:{angle}";
+            }
+            int min = 0;
+            int hour = 0;
+            double angle1 = angle;
+            string result = "";
+            angle += angle;
+            hour = (int)angle / 60;
+            bool hourIsExist = hour > 0;
+            if (hour <= 9)
+            {
+                result = $"0{hour}:";
+            }
+            else
+            {
+                result = $"{hour}:";
+            }
+            if (hourIsExist)
+            {
+                min = (int)angle - (60 * hour);
+                if (min <= 9)
+                {
+                    result += $"0{min}";
+                }
+                else
+                    result += $"{min}";
+            }
+            else
+            {
+                min = (int)angle;
+                result += $"{min}";
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/54e6533c92449cc251001667/
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+
+            T previous = default(T);
+            foreach (T current in iterable)
+            {
+                if (!current.Equals(previous))
+                {
+                    previous = current;
+                    yield return current;
+                }
+            }
+        }
+
+        #endregion
+
+        #region https://www.codewars.com/kata/5544c7a5cb454edb3c000047/
+        public static int bouncingBall(double h, double bounce, double window)
+        {
+            int count = 0;
+            if (h > 0 && bounce > 0 && bounce < 1 && h > window)
+            {
+                while (h >= window)
+                {
+                    count++;
+                    h *= bounce;
+                    if (h > window)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2) { return 1; }
+                else
+                {
+                    return count;
+                }
+            }
+            else { return -1; }
+        }
+        #endregion
+
     }
 }
